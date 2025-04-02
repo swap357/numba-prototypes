0b52daf5e0a7166c8d05f49036fbe487d0aba72b
Siu Kwan Lam <1929845+sklam@users.noreply.github.com>
2025-04-02 15:09:12 -0500

    Separate out the tests. Add descriptions to notebooks

--- sealir-tutorials/ch01_basic_compiler.py
+++ sealir-tutorials/ch01_basic_compiler.py
@@ -7,11 +7,10 @@
 #       format_version: '1.5'
 #       jupytext_version: 1.16.7
 #   kernelspec:
-#     display_name: Python 3
+#     display_name: sealir_basic_compiler
 #     language: python
 #     name: python3
 # ---
-
 
 # # Ch 1. Basic Compiler
 #
@@ -35,6 +34,16 @@ from sealir.llvm_pyapi_backend import (
 from sealir.rvsdg import grammar as rg
 
 # ## The frontend
+#
+# The frontend accepts a Python function object, reads its source code, and
+# parses its Abstract Syntax Tree (AST). It then transforms the AST into a
+# Regionalized Value-State Dependence Graph (RVSDG), a representation that
+# simplifies further intermediate representation (IR) processing. The RVSDG uses
+# a data-flow centric encoding in which control-flow constructs are mapped into
+# regions. These regions function much like ordinary operations, with clearly
+# defined sets of input and output ports. Additionally, state is explicitly
+# encoded as I/O, so that every operation maintains a pure operational
+# appearance.
 
 
 def frontend(fn):
@@ -46,7 +55,95 @@ def frontend(fn):
     return rvsdg_expr, dbginfo
 
 
+# Here's a simple function to illustrate the frontend
+
+if __name__ == "__main__":
+
+    def exercise_frontend_simple(x, y):
+        return x + y
+
+    rvsdg_expr, dbg = frontend(exercise_frontend_simple)
+    print(rvsdg.format_rvsdg(rvsdg_expr))
+
+# The function’s RVSDG-IR includes a region:
+#
+# ```
+# $0 = Region[236] <- !io x y
+# {
+#     ...
+# } [302] -> !io=$1[0] !ret=$1[1] x=$0[1] y=$0[2]
+# ```
+#
+# The region has three input ports: `!io`, `x`, and `y`.
+# Its output ports are `!io`, `!ret`, `x`, and `y`.
+#
+# Here, `!io` represents the state. Because Python functions are imperative,
+# each function carries an implicit state that must be updated for any
+# side-effect.
+#
+# The `!ret` port holds the function’s return value.
+#
+# The `x` and `y` output ports convey the region’s internal value state, but the
+# function node ignores these values.
+#
+# The single operation in the function is:
+#
+# ```
+#  $1 = PyBinOp + $0[0] $0[1], $0[2]
+# ```
+#
+# This is a Python binary operation. It uses input operands from `$0`, which is
+# the header of the function’s region. In this notation, `$0[n]` refers to a
+# specific port: `$0[0]` corresponds to `!io`, `$0[1]` to `x`, and `$0[2]` to
+# `y`.
+#
+#
+
+# Below is a more intricate example that requires restructuring of the
+# control flow. This is due to the use of the `break` statement to exit a
+# for-loop.
+#
+# RVSDG enforces structured control flow. Only three types of control-flow
+# regions are permitted:
+#
+# - Linear region, as `Region {...}`;
+# - If-Else region, as `If {...} Else {...} EndIf`;
+# - Loop region, as `Loop {...} EndLoop`.
+
+if __name__ == "__main__":
+
+    def exercise_frontend_loop_if_break(x, y):
+        c = 0
+        for i in range(x):
+            if i > y:
+                break
+            c += i
+        return c
+
+    rvsdg_expr, dbg = frontend(exercise_frontend_loop_if_break)
+    print(rvsdg.format_rvsdg(rvsdg_expr))
+
+
+# Observations from the RVSDG-IR above:
+#
+# * The for-loop is restructured into a `Loop` composed of `If-Else` regions.
+# * The `Loop` region is tail-controlled; its first output port acts as the loop
+#   condition. See `!_loopcond_0002`.
+# * An extra `If-Else` follows the loop to adjust the value of `i`.
+#
+# The beauty of the structured control-flow is that everything can be
+# encapsulated as a plain data-flow operation, including any region.
+# Everything is just an operation with some input and output ports.
+# This simplifies the rest of the compiler.
+
+# ## The backend
+#
+# SealIR includes a lightweight LLVM backend that emits the Python C-API, which
+# executes the RVSDG-IR. The example below demonstrates how to use this backend
+
+
 def _determine_arity(root: ase.SExpr) -> int:
+    """Helper function to get the arity of the function node"""
     match root:
         case rg.Func(args=rg.Args() as args):
             return len(args.arguments)
@@ -109,6 +206,9 @@ def backend(root, ns=builtins.__dict__):
     return mod
 
 
+# The following function takes a LLVM module and JIT compile it for execution:
+
+
 def jit_compile(mod, rvsdg_expr):
     """JIT compile LLVM module into an executable function for this process."""
     llvm_ir = str(mod)
@@ -127,7 +227,10 @@ def jit_compile(mod, rvsdg_expr):
     return JitCallable.from_pointer(rt, ptr, arity)
 
 
-def compiler_pipeline(fn, args, *, verbose=False):
+# The following is a simple compiler pipeline:
+
+
+def compiler_pipeline(fn, *, verbose=False):
     rvsdg_expr, dbginfo = frontend(fn)
 
     if verbose:
@@ -144,6 +247,10 @@ def compiler_pipeline(fn, args, *, verbose=False):
         print(llmod)
 
     jt = jit_compile(llmod, rvsdg_expr)
+    return jt
+
+
+def run_test(fn, jt, args, *, verbose=False):
     res = jt(*args)
 
     if verbose:
@@ -151,38 +258,20 @@ def compiler_pipeline(fn, args, *, verbose=False):
         print(res)
 
     assert res == fn(*args)
+    return res
 
 
-def test_ch01_sum_ints():
-    def sum_ints(n):
-        c = 0
-        for i in range(n):
-            c += i
-        return c
-
-    compiler_pipeline(sum_ints, (12,), verbose=False)
-
-
-def test_ch01_max_two():
-    def max_if_else(x, y):
-        if x > y:
-            return x
-        else:
-            return y
-
-    compiler_pipeline(max_if_else, (1, 2), verbose=False)
-    compiler_pipeline(max_if_else, (3, 2), verbose=False)
-
-
-def main():
-    def sum_ints(n):
-        c = 0
-        for i in range(n):
-            c += i
-        return c
-
-    compiler_pipeline(sum_ints, (12,), verbose=True)
-
+# The following puts everything together. Running the frontend to generate
+# RVSDG-IR. Then, emitting LLVM using the backend. Finally, JIT'ing it into
+# executable code and verifying it.
 
 if __name__ == "__main__":
-    main()
+
+    def sum_ints(n):
+        c = 0
+        for i in range(n):
+            c += i
+        return c
+
+    jt = compiler_pipeline(sum_ints, verbose=True)
+    run_test(sum_ints, jt, (12,), verbose=True)
---
+++ sealir-tutorials/tests/test_ch01.py
@@ -0,0 +1,26 @@
+from ch01_basic_compiler import *
+
+
+def test_ch01_sum_ints():
+    def sum_ints(n):
+        c = 0
+        for i in range(n):
+            c += i
+        return c
+
+    jt = compiler_pipeline(sum_ints)
+    run_test(sum_ints, jt, (12,))
+
+
+def test_ch01_max_two():
+    def max_if_else(x, y):
+        if x > y:
+            return x
+        else:
+            return y
+
+    jt = compiler_pipeline(max_if_else)
+    args = (1, 2)
+    run_test(max_if_else, jt, args)
+    args = (3, 2)
+    run_test(max_if_else, jt, args)
