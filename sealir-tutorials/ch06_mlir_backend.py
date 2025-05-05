0822e7582e3aecf526cb9529630e0b88ab74c186
kc611 <ckaustubhm06@gmail.com>
2025-05-05 21:42:53 +0530

    Refactor chapter 6 and add tests

--- sealir-tutorials/ch06_mlir_backend.py
+++ sealir-tutorials/ch06_mlir_backend.py
@@ -70,6 +70,13 @@ from ch04_2_typeinfer_loops import (
 
 from utils import IN_NOTEBOOK
 
+@dataclass(frozen=True)
+class LowerStates(ase.TraverseState):
+    push: Callable
+    get_region_args: Callable
+    function_block: func.FuncOp
+    constant_block: ir.Block
+
 class Backend:
     def lower_type(self, ty: NbOp_Type):
         match ty:
@@ -90,10 +97,10 @@ class Backend:
         self.module = module = ir.Module.create(loc=loc)
 
         # f32 = ir.F32Type.get(context=context)
-        self.f64 = f64 = ir.F64Type.get(context=context)
-        self.i32 = i32 = ir.IntegerType.get_signless(32, context=context)
-        self.i64 = i64 = ir.IntegerType.get_signless(64, context=context)
-        self.boo = boo = ir.IntegerType.get_signless(1, context=context)
+        self.f64 = ir.F64Type.get(context=context)
+        self.i32 = ir.IntegerType.get_signless(32, context=context)
+        self.i64 = ir.IntegerType.get_signless(64, context=context)
+        self.boo = ir.IntegerType.get_signless(1, context=context)
 
         module_body = ir.InsertionPoint(module.body)
         input_types = tuple([self.get_mlir_type(x) for x in argtypes])
@@ -123,196 +130,8 @@ class Backend:
         def get_region_args():
             return region_args[-1]
         
-        def lower_expr(expr: SExpr, state: ase.TraverseState):
-            match expr:
-                case rg.Func(args=args, body=body):
-                    names = {
-                        argspec.name: fun.arguments[i]
-                        for i, argspec in enumerate(args.arguments)
-                    }
-                    argvalues = []
-                    for k in body.begin.inports:
-                        if k == internal_prefix("io"):
-                            v = arith.constant(i32, 0)
-                        else:
-                            v = names[k]
-                        argvalues.append(v)
-
-                    with push(argvalues):
-                        outs = yield body
-
-                    portnames = [p.name for p in body.ports]
-                    retval = outs[portnames.index(internal_prefix("ret"))]
-                    global func
-                    func.ReturnOp([retval])
-                case rg.RegionBegin(inports=ins):
-                    portvalues = []
-                    for i, k in enumerate(ins):
-                        pv = get_region_args()[i]
-                        portvalues.append(pv)
-                    return tuple(portvalues)
-
-                case rg.RegionEnd(
-                    begin=rg.RegionBegin() as begin,
-                    ports=ports,
-                ):
-                    yield begin
-                    portvalues = []
-                    for p in ports:
-                        pv = yield p.value
-                        portvalues.append(pv)
-                    return tuple(portvalues)
-
-                case rg.ArgRef(idx=int(idx), name=str(name)):
-                    return fun.arguments[idx]
-
-                case rg.Unpack(val=source, idx=int(idx)):
-                    ports = yield source
-                    return ports[idx]
-
-                case rg.DbgValue(value=value):
-                    val = yield value
-                    return val
-
-                case rg.PyInt(int(ival)):
-                    with constant_entry:
-                        const = arith.constant(self.i64, ival)
-                    return const
-                
-                case rg.PyBool(int(ival)):
-                    with constant_entry:
-                        const = arith.constant(self.boo, ival)
-                    return const
-
-                case rg.PyFloat(float(fval)):
-                    with constant_entry:
-                        const = arith.constant(self.f64, fval)
-                    return const
-
-                case NbOp_Gt_Int64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-                    return arith.cmpi(4, lhs, rhs)
-
-                case NbOp_Add_Int64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-                    return arith.addi(lhs, rhs)
-
-                case NbOp_Sub_Int64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-                    return arith.subi(lhs, rhs)
-
-                case NbOp_Add_Float64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-                    return arith.addf(lhs, rhs)
-                case NbOp_Sub_Float64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-                    return arith.subf(lhs, rhs)
-                case NbOp_Lt_Int64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-                    return arith.cmpi(2, lhs, rhs)
-                case NbOp_Sub_Int64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-                    return arith.subi(lhs, rhs)
-
-                case NbOp_CastI64ToF64(operand):
-                    val = yield operand
-                    return arith.sitofp(self.f64, val)
-                case NbOp_Div_Int64(lhs, rhs):
-                    lhs = yield lhs
-                    rhs = yield rhs
-
-                    return arith.sitofp(self.f64, arith.divsi(lhs, rhs))
-                ##### more
-                case NbOp_Not_Int64(operand):
-                    # Implement unary not
-                    opval = yield operand
-                    return arith.cmpi(0, opval, arith.constant(self.i64, 0))
-                case rg.PyBool(val):
-                    return arith.constant(self.boo, val)
-
-                case rg.PyInt(val):
-                    return arith.constant(self.i64, val)
-
-                case rg.IfElse(cond=cond, body=body, orelse=orelse, operands=operands):
-                    condval = (yield cond)
-
-                    # process operands
-                    rettys = Attributes(body.begin.attrs)
-                    result_tys = []
-                    for i in range(0, rettys.num_output_types()+1):
-                        out_ty = rettys.get_output_type(i)
-                        if out_ty is not None:
-                            match out_ty.name:
-                                case "Int64":
-                                    result_tys.append(self.i64)
-                                case "Float64":
-                                    result_tys.append(self.f64)
-                                case "Bool":
-                                    result_tys.append(self.boo)
-                        else:
-                            result_tys.append(self.i32)
-
-                    if_op = scf.IfOp(cond=condval, results_= result_tys, hasElse=bool(orelse))
-
-                    with ir.InsertionPoint(if_op.then_block):
-                        value_else = yield body
-                        scf.YieldOp([x for x in value_else])
-
-                    with ir.InsertionPoint(if_op.else_block):
-                        value_else = yield orelse
-                        scf.YieldOp([x for x in value_else])
-
-                    return if_op.results
-                case rg.Loop(body=rg.RegionEnd() as body, operands=operands):
-                    rettys = Attributes(body.begin.attrs)
-                    # process operands
-                    ops = []
-                    for op in operands:
-                        ops.append((yield op))
-
-                    result_tys = []
-                    for i in range(1, rettys.num_output_types()+1):
-                        out_ty = rettys.get_output_type(i)
-                        if out_ty is not None:
-                            match out_ty.name:
-                                case "Int64":
-                                    result_tys.append(self.i64)
-                                case "Float64":
-                                    result_tys.append(self.f64)
-                                case "Bool":
-                                    result_tys.append(self.boo)
-                        else:
-                            result_tys.append(self.i32)
-
-                    while_op = scf.WhileOp(results_=result_tys, inits=[op for op in ops])
-                    before_block = while_op.before.blocks.append(*result_tys)
-                    after_block = while_op.after.blocks.append(*result_tys)
-                    new_ops = before_block.arguments
-        
-                    # Before Region
-                    with ir.InsertionPoint(before_block), push(new_ops):
-                        values = yield body
-                        scf.ConditionOp(args=[val for val in values[1:]], condition=values[0])
-
-                    # After Region
-                    with ir.InsertionPoint(after_block):
-                        scf.YieldOp(after_block.arguments)
-
-                    while_op_res = scf._get_op_results_or_values(while_op)
-                    return while_op_res
-
-                case _:
-                    raise NotImplementedError(expr, type(expr))
-
         with context, loc, function_entry:
-            memo = ase.traverse(root, lower_expr)
+            memo = ase.traverse(root, self.lower_expr, LowerStates(push=push, get_region_args=get_region_args, function_block=fun, constant_block=constant_entry))
 
         with context, loc, constant_entry:
             cf.br([], fun.body.blocks[1])
@@ -326,6 +145,194 @@ class Backend:
         module.dump()
 
         return module
+
+    def lower_expr(self, expr: SExpr, state: LowerStates):
+        match expr:
+            case rg.Func(args=args, body=body):
+                names = {
+                    argspec.name: state.function_block.arguments[i]
+                    for i, argspec in enumerate(args.arguments)
+                }
+                argvalues = []
+                for k in body.begin.inports:
+                    if k == internal_prefix("io"):
+                        v = arith.constant(self.i32, 0)
+                    else:
+                        v = names[k]
+                    argvalues.append(v)
+
+                with state.push(argvalues):
+                    outs = yield body
+
+                portnames = [p.name for p in body.ports]
+                retval = outs[portnames.index(internal_prefix("ret"))]
+                global func
+                func.ReturnOp([retval])
+            case rg.RegionBegin(inports=ins):
+                portvalues = []
+                for i, k in enumerate(ins):
+                    pv = state.get_region_args()[i]
+                    portvalues.append(pv)
+                return tuple(portvalues)
+
+            case rg.RegionEnd(
+                begin=rg.RegionBegin() as begin,
+                ports=ports,
+            ):
+                yield begin
+                portvalues = []
+                for p in ports:
+                    pv = yield p.value
+                    portvalues.append(pv)
+                return tuple(portvalues)
+
+            case rg.ArgRef(idx=int(idx), name=str(name)):
+                return state.function_block.arguments[idx]
+
+            case rg.Unpack(val=source, idx=int(idx)):
+                ports = yield source
+                return ports[idx]
+
+            case rg.DbgValue(value=value):
+                val = yield value
+                return val
+
+            case rg.PyInt(int(ival)):
+                with state.constant_block:
+                    const = arith.constant(self.i64, ival)
+                return const
+            
+            case rg.PyBool(int(ival)):
+                with state.constant_block:
+                    const = arith.constant(self.boo, ival)
+                return const
+
+            case rg.PyFloat(float(fval)):
+                with state.constant_block:
+                    const = arith.constant(self.f64, fval)
+                return const
+
+            case NbOp_Gt_Int64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+                return arith.cmpi(4, lhs, rhs)
+
+            case NbOp_Add_Int64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+                return arith.addi(lhs, rhs)
+
+            case NbOp_Sub_Int64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+                return arith.subi(lhs, rhs)
+
+            case NbOp_Add_Float64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+                return arith.addf(lhs, rhs)
+            case NbOp_Sub_Float64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+                return arith.subf(lhs, rhs)
+            case NbOp_Lt_Int64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+                return arith.cmpi(2, lhs, rhs)
+            case NbOp_Sub_Int64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+                return arith.subi(lhs, rhs)
+
+            case NbOp_CastI64ToF64(operand):
+                val = yield operand
+                return arith.sitofp(self.f64, val)
+            case NbOp_Div_Int64(lhs, rhs):
+                lhs = yield lhs
+                rhs = yield rhs
+
+                return arith.divf(arith.sitofp(self.f64, lhs), arith.sitofp(self.f64, rhs))
+            ##### more
+            case NbOp_Not_Int64(operand):
+                # Implement unary not
+                opval = yield operand
+                return arith.cmpi(0, opval, arith.constant(self.i64, 0))
+            case rg.PyBool(val):
+                return arith.constant(self.boo, val)
+
+            case rg.PyInt(val):
+                return arith.constant(self.i64, val)
+
+            case rg.IfElse(cond=cond, body=body, orelse=orelse, operands=operands):
+                condval = (yield cond)
+
+                # process operands
+                rettys = Attributes(body.begin.attrs)
+                result_tys = []
+                for i in range(0, rettys.num_output_types()+1):
+                    out_ty = rettys.get_output_type(i)
+                    if out_ty is not None:
+                        match out_ty.name:
+                            case "Int64":
+                                result_tys.append(self.i64)
+                            case "Float64":
+                                result_tys.append(self.f64)
+                            case "Bool":
+                                result_tys.append(self.boo)
+                    else:
+                        result_tys.append(self.i32)
+
+                if_op = scf.IfOp(cond=condval, results_= result_tys, hasElse=bool(orelse))
+
+                with ir.InsertionPoint(if_op.then_block):
+                    value_else = yield body
+                    scf.YieldOp([x for x in value_else])
+
+                with ir.InsertionPoint(if_op.else_block):
+                    value_else = yield orelse
+                    scf.YieldOp([x for x in value_else])
+
+                return if_op.results
+            case rg.Loop(body=rg.RegionEnd() as body, operands=operands):
+                rettys = Attributes(body.begin.attrs)
+                # process operands
+                ops = []
+                for op in operands:
+                    ops.append((yield op))
+
+                result_tys = []
+                for i in range(1, rettys.num_output_types()+1):
+                    out_ty = rettys.get_output_type(i)
+                    if out_ty is not None:
+                        match out_ty.name:
+                            case "Int64":
+                                result_tys.append(self.i64)
+                            case "Float64":
+                                result_tys.append(self.f64)
+                            case "Bool":
+                                result_tys.append(self.boo)
+                    else:
+                        result_tys.append(self.i32)
+
+                while_op = scf.WhileOp(results_=result_tys, inits=[op for op in ops])
+                before_block = while_op.before.blocks.append(*result_tys)
+                after_block = while_op.after.blocks.append(*result_tys)
+                new_ops = before_block.arguments
+    
+                # Before Region
+                with ir.InsertionPoint(before_block), state.push(new_ops):
+                    values = yield body
+                    scf.ConditionOp(args=[val for val in values[1:]], condition=values[0])
+
+                # After Region
+                with ir.InsertionPoint(after_block):
+                    scf.YieldOp(after_block.arguments)
+
+                while_op_res = scf._get_op_results_or_values(while_op)
+                return while_op_res
+
+            case _:
+                raise NotImplementedError(expr, type(expr))
 
     def jit_compile(self, llmod, func_node: rg.Func):
         attributes = Attributes(func_node.body.begin.attrs)
@@ -442,7 +449,7 @@ if __name__ == "__main__":
         backend=Backend(),
     )
     args = (10, 33)
-    # run_test(example_2, jt, args, verbose=True)
+    run_test(example_2, jt, args, verbose=True)
     args = (7, 3)
     run_test(example_2, jt, args, verbose=True)
 
@@ -490,10 +497,10 @@ if __name__ == "__main__":
         assert "Failed to unify if-else outgoing variables: z" in str(e)
 
 
-# ## Example 1: Simple while loop example
+# ## Example 4: Simple while loop example
 
 
-def example_1(init, n):
+def example_4(init, n):
     c = float(init)
     i = 0
     while i < n:
@@ -504,7 +511,7 @@ def example_1(init, n):
 
 if __name__ == "__main__":
     jt = compiler_pipeline(
-        example_1,
+        example_4,
         argtypes=(Int64, Int64),
         ruleset=loop_ruleset,
         verbose=True,
@@ -512,13 +519,13 @@ if __name__ == "__main__":
         cost_model=MyCostModel(),
         backend=Backend(),
     )
-    run_test(example_1, jt, (10, 7), verbose=True)
+    run_test(example_4, jt, (10, 7), verbose=True)
 
 
-# ## Example 2: Nested Loop example
+# ## Example 5: Nested Loop example
 
 
-def example_2(init, n):
+def example_5(init, n):
     c = float(init)
     i = 0
     while i < n:
@@ -532,7 +539,7 @@ def example_2(init, n):
 
 if __name__ == "__main__":
     jt = compiler_pipeline(
-        example_2,
+        example_5,
         argtypes=(Int64, Int64),
         ruleset=loop_ruleset,
         verbose=True,
@@ -540,4 +547,4 @@ if __name__ == "__main__":
         cost_model=MyCostModel(),
         backend=Backend(),
     )
-    run_test(example_2, jt, (10, 7), verbose=True)
+    run_test(example_5, jt, (10, 7), verbose=True)
---
+++ sealir-tutorials/tests/test_ch06.py
@@ -0,0 +1,88 @@
+from sealir import ase
+
+from ch06_mlir_backend import *
+
+
+
+def test_ch06_example_1():
+    jt = compiler_pipeline(
+        example_1,
+        argtypes=(Int64, Int64),
+        ruleset=(if_else_ruleset | facts_function_types),
+        verbose=False,
+        converter_class=ConditionalExtendGraphtoRVSDG,
+        cost_model=MyCostModel(),
+        backend=Backend(),
+    )
+    args = (10, 33)
+    run_test(example_1, jt, args, verbose=False)
+    args = (7, 3)
+    run_test(example_1, jt, args, verbose=False)
+
+
+def test_ch06_example_2():
+    jt = compiler_pipeline(
+        example_2,
+        argtypes=(Int64, Int64),
+        ruleset=(
+            if_else_ruleset
+            | facts_function_types
+            | ruleset_type_infer_float  # < --- added for float()
+        ),
+        verbose=False,
+        converter_class=ConditionalExtendGraphtoRVSDG,
+        cost_model=MyCostModel(),
+        backend=Backend(),
+    )
+    args = (10, 33)
+    run_test(example_2, jt, args, verbose=False)
+    args = (7, 3)
+    run_test(example_2, jt, args, verbose=False)
+
+
+def test_ch06_example_3():
+    try:
+        compiler_pipeline(
+            example_3,
+            argtypes=(Int64, Int64),
+            ruleset=(
+                if_else_ruleset
+                | facts_function_types
+                | ruleset_type_infer_float
+                | ruleset_failed_to_unify
+            ),
+            verbose=False,
+            converter_class=ConditionalExtendGraphtoRVSDG,
+            cost_model=MyCostModel(),
+            backend=Backend(),
+        )
+    except CompilationError as e:
+        # Compilation failed because the return type cannot be determined.
+        # This indicates that the type inference is incomplete
+        print_exception(e)
+        assert "fail to unify" in str(e)
+
+def test_ch06_example_4():
+    jt = compiler_pipeline(
+        example_4,
+        argtypes=(Int64, Int64),
+        ruleset=loop_ruleset,
+        verbose=False,
+        converter_class=LoopExtendEGraphToRVSDG,
+        cost_model=MyCostModel(),
+        backend=Backend(),
+    )
+    run_test(example_4, jt, (10, 7), verbose=False)
+
+
+def test_ch06_example_5():
+    jt = compiler_pipeline(
+        example_5,
+        argtypes=(Int64, Int64),
+        ruleset=loop_ruleset,
+        verbose=False,
+        converter_class=LoopExtendEGraphToRVSDG,
+        cost_model=MyCostModel(),
+        backend=Backend(),
+    )
+    run_test(example_5, jt, (10, 7), verbose=False)
